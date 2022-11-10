8d4fe7c1089149a8c498d790f2e97814039267a2
Spencer Hayes <113189856+spencerschoolaccount@users.noreply.github.com>
2022-11-10 15:51:33 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+pizzaSize = input('Would you like a large or an extra large pizza?')
+
+if pizzaSize.lower() == 'large':
+  pizzaCost = 6.00
+elif pizzaSize.lower() == 'extra large':
+  pizzaCost = 10.00
+else:
+  exit('Invalid input, please restart program')
+
+toppingAmount = input('How many toppings would you like? (1, 2, 3, or 4')
+
+if toppingAmount == 1:
+  toppingCost = 1.00
+elif toppingAmount == 2:
+  toppingCost = 1.75
+elif toppingAmount == 3:
+  toppingCost = 2.50
+elif toppingAmount == 4:
+  toppingCost = 3.35
+else:
+  exit('Invalid input, please restart program')
+
+Subtotal = pizzaCost + toppingCost
+print("""Your receipt is:
+Subtotal: $""" + string(Subtotal) + """
+Tax: $""" + string(round(Subtotal*0.13,2)) + """
+Total: $"""
+""")
