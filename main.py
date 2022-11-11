f389ba0f192ec5b89ee80d5067a5c0376d663673
Spencer Hayes <113189856+spencerschoolaccount@users.noreply.github.com>
2022-11-11 15:36:02 +0000

    Finished

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-pizzaSize = input('Would you like a large or an extra large pizza?')
+pizzaSize = input('Would you like a large or an extra large pizza? \n')
 
 if pizzaSize.lower() == 'large':
   pizzaCost = 6.00
@@ -7,22 +7,22 @@ elif pizzaSize.lower() == 'extra large':
 else:
   exit('Invalid input, please restart program')
 
-toppingAmount = input('How many toppings would you like? (1, 2, 3, or 4')
+toppingAmount = input('How many toppings would you like? (1, 2, 3, or 4 \n')
 
-if toppingAmount == 1:
+if toppingAmount == '1':
   toppingCost = 1.00
-elif toppingAmount == 2:
+elif toppingAmount == '2':
   toppingCost = 1.75
-elif toppingAmount == 3:
+elif toppingAmount == '3':
   toppingCost = 2.50
-elif toppingAmount == 4:
+elif toppingAmount == '4':
   toppingCost = 3.35
 else:
   exit('Invalid input, please restart program')
 
 Subtotal = pizzaCost + toppingCost
 print("""Your receipt is:
-Subtotal: $""" + string(Subtotal) + """
-Tax: $""" + string(round(Subtotal*0.13,2)) + """
-Total: $"""
-""")
+Subtotal: $""" + str(Subtotal) + """
+Tax: $""" + str(round(Subtotal*0.13,2)) + """
+Total: $""" + str(round(Subtotal*1.13,2))
+)
